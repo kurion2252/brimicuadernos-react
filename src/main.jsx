114e4c91adfcfd6bbd35948604b712437b38096d
kurion2252 <120735031+kurion2252@users.noreply.github.com>
2025-10-14 14:15:44 -0300

    nuevos cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot} from 'react-dom/client';
-import './index.css';
 import App from './App';
-// import './src/assest/styles/index.css';
+import '../src/assest/styles/style.css';
 
 createRoot(document.getElementById('root')).render(<App />);
